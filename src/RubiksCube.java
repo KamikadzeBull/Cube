f4f9ed17d9ea92e41bb361d42546c65f88e0742a
KamikadzeBull <kenny1634tlt@yandex.ru>
2018-03-06 10:11:41 +0300

    Немного дефолтовых методов

--- src/RubiksCube.java
+++ src/RubiksCube.java
@@ -19,4 +19,32 @@ public class RubiksCube {
                 for (int column = 0; column < size; column++)
                     cube[sideID][row][column] = sideID;
     }
+
+    public void show(){
+        System.out.print("front");
+        for (int count = 0; count < size-1; count++)
+            System.out.print(" ");
+        System.out.print("back");
+        for (int count = 0; count < size; count++)
+            System.out.print(" ");
+        System.out.print("left");
+        for (int count = 0; count < size; count++)
+            System.out.print(" ");
+        System.out.print("right");
+        for (int count = 0; count < size-1; count++)
+            System.out.print(" ");
+        System.out.print("top");
+        for (int count = 0; count < size+1; count++)
+            System.out.print(" ");
+        System.out.println("button");
+        for (int row = 0; row < size; row++) {
+            for (int sideID = 0; sideID < 6; sideID++) {
+                for (int column = 0; column < size; column++)
+                    System.out.print(cube[sideID][row][column]);
+                for (int count = 0; count < size+1; count++)
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
 }
