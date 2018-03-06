8f7752e82c39c0b891b09a48413b51a426e56fc8
KamikadzeBull <kenny1634tlt@yandex.ru>
2018-03-06 11:15:00 +0300

    Далее будет мясо🥩

--- src/RubiksCube.java
+++ src/RubiksCube.java
@@ -1,10 +1,14 @@
+import java.util.*;
+
 public class RubiksCube {
 
+    // дефолтовый размер
     public RubiksCube(){
         size = 3;
         setDefaultColors();
     }
 
+    // кастомный размер
     public RubiksCube(int size){
         this.size = size;
         setDefaultColors();
@@ -13,6 +17,7 @@ public class RubiksCube {
     private int size;
     private int[][][] cube = new int[6][size][size];
 
+    // возвращение куба в собранный вид
     public void setDefaultColors(){
         for (int sideID = 0; sideID < 6; sideID++)
             for (int row = 0; row < size; row++)
@@ -20,6 +25,7 @@ public class RubiksCube {
                     cube[sideID][row][column] = sideID;
     }
 
+    // отображение куба
     public void show(){
         System.out.print("front");
         for (int count = 0; count < size-1; count++)
@@ -46,5 +52,57 @@ public class RubiksCube {
             }
             System.out.println();
         }
+    }
+
+    // установление соответствия между строковыми параметрами и индексами сторон
+    private static int getSideID (String side){
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("front", 0);    // white
+        map.put("back", 1);     // yellow
+        map.put("left", 2);     // orange
+        map.put("right", 3);    // red
+        map.put("top", 4);      // blue
+        map.put("bottom", 5);   // green
+        return map.get(side);
+    }
+
+    /* соседние стороны:
+    *       - индексы массива ~ индексы сторон
+    *       - элемент массива ~ ряд индексов сторон; формально: представим сторону
+    *           как находящуюся прямо перед глазами, тогда ряд индексов является индексами
+    *           соседней верхней, правой, нижней и левой сторон
+    */
+    private static int[] nearSidesID (int sideID) {
+        int[][] nearSidesID = {{4,3,5,2}, {4,2,5,3},
+                {4,0,5,1}, {4,1,5,0}, {1,3,0,2}, {1,2,0,3}};
+        return nearSidesID[sideID];
+    }
+
+    /* вращение грани без взаимодействия с другими гранями:
+    *       - сторона якобы находится перед глазами
+    *       - неважно, каково вращение с постороннего взгляда
+    */
+    private void sideRotation(int sideID, String direction){
+        int[][] oldSide = cube[sideID];
+        switch (direction){
+            case "clockwise":
+                for (int i = 0; i < size; i++)
+                    for (int j = 0; j < size; j++)
+                        cube[sideID][j][(size - 1) - i] = oldSide[i][j];
+                break;
+            case "anticlockwise":
+                for (int i = 0; i < size; i++)
+                    for (int j = 0; j < size; j++)
+                        cube[sideID][(size - 1) - j][i] = oldSide[i][j];
+                break;
+        }
+    }
+
+    // подобие двойного выполнения sideRotation() по одному направлению
+    private void sideInversion(int sideID){
+        int[][] oldSide = cube[sideID];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                cube[sideID][(size - 1) - i][(size - 1) - i] = oldSide[i][j];
     }
 }
