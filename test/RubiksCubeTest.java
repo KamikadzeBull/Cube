d893fa6254e0960fd6cbcdc34dc1a9420faa0fba
KamikadzeBull <kenny1634tlt@yandex.ru>
2018-03-08 10:05:57 +0300

    Все-таки здесь будут тесты

---
+++ test/RubiksCubeTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RubiksCubeTest {
+
+    @Test
+    void setDefaultColors() {
+    }
+
+    @Test
+    void show() {
+    }
+
+    @Test
+    void cubeRotation() {
+    }
+
+    @Test
+    void layerRotation() {
+    }
+}
